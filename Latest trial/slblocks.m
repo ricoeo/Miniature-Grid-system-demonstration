69e1678b6dfb9eb39979e286da3ec7ff01db39b7
ZHAO MING <36293694+ricoeo@users.noreply.github.com>
2024-01-15 15:21:43 +0100

    update the model
    Change topology and update library

---
+++ Latest trial/slblocks.m
@@ -0,0 +1,11 @@
+function blkStruct = slblocks
+% This function specifies that the library 'DC_GRIDSIM_LIB'
+% should appear in the Library Browser with the 
+% name 'DC GRIDSIM Library'
+
+    Browser.Library = 'DC_GRIDSIM_LIB';
+
+    Browser.Name = 'DC GRIDSIM Library';
+
+    blkStruct.Browser = Browser;
+end
